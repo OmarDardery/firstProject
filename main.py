d83aeee4c93e2668d7ca8f30f6ed89b24c50e15c
Omar Dardery <oadardery@gmail.com>
2025-06-07 22:24:35 +0300

    first push

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+import os
+from supabase import create_client, Client
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
